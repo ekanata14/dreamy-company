e6a4c8f0d1db7c22d6868054c81e63932f7fdfea
ekanata14 <ekanata1411@gmail.com>
2023-10-13 19:46:14 +0800

    Add effect ease in and on scroll effect, add text on our features

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,29 @@
-function typeWriterEffect() {
+// Variable for getting all element with featureImg id
+const targets = document.querySelectorAll('#featureImg');
+
+// Lazy Load function to load the image when the image is viewed
+const lazyLoad = (target) => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+                img.classList.add('element-ease-in-transition');
+
+                observer.disconnect();
+            }
+        });
+    });
+    io.observe(target);
+}
+
+// Make the function run on foreach lazyload
+targets.forEach(lazyLoad);
+
+// Function for typewriter Effect
+const typeWriterEffect = () => {
     
     // Declaring Variables
     const words = ["Friends",  "Assistant", "Teacher"];
@@ -43,4 +68,5 @@ function typeWriterEffect() {
     }
     type();
 }
+
 typeWriterEffect();
